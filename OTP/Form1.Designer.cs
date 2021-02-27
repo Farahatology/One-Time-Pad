5810dd60804e14a37d6e22b6d66a1448e8455e2e
Ahmed Farahat <dr_it@live.com>
2021-02-27 03:59:42 +0200

    Add project files.

---
+++ OTP/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace OTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxPad = new System.Windows.Forms.TextBox();
+            this.textBoxOriginal = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.textBoxDecrypted = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(590, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(590, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxPad
+            // 
+            this.textBoxPad.Location = new System.Drawing.Point(149, 56);
+            this.textBoxPad.Name = "textBoxPad";
+            this.textBoxPad.Size = new System.Drawing.Size(421, 26);
+            this.textBoxPad.TabIndex = 1;
+            // 
+            // textBoxOriginal
+            // 
+            this.textBoxOriginal.Location = new System.Drawing.Point(149, 104);
+            this.textBoxOriginal.Name = "textBoxOriginal";
+            this.textBoxOriginal.Size = new System.Drawing.Size(421, 26);
+            this.textBoxOriginal.TabIndex = 1;
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.Location = new System.Drawing.Point(149, 153);
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.Size = new System.Drawing.Size(421, 26);
+            this.textBoxEncrypted.TabIndex = 1;
+            // 
+            // textBoxDecrypted
+            // 
+            this.textBoxDecrypted.Location = new System.Drawing.Point(149, 201);
+            this.textBoxDecrypted.Name = "textBoxDecrypted";
+            this.textBoxDecrypted.Size = new System.Drawing.Size(421, 26);
+            this.textBoxDecrypted.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Plain text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cipher text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Decrypted Text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 299);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDecrypted);
+            this.Controls.Add(this.textBoxEncrypted);
+            this.Controls.Add(this.textBoxOriginal);
+            this.Controls.Add(this.textBoxPad);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "One Time Pad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxPad;
+        private System.Windows.Forms.TextBox textBoxOriginal;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.TextBox textBoxDecrypted;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ OTP/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] pad;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Converting text to bytes, assuming unicode.
+            byte[] originalBytes = Encoding.Unicode.GetBytes(textBoxOriginal.Text);
+
+            // generate a pad in memory.
+            pad = GeneratePad(size: originalBytes.Length, seed: 1);
+
+            // I'm going to display these bytes in Base64, but one would
+            // probably save them to a file; this is the Pad (or "key").
+            textBoxPad.Text = Convert.ToBase64String(inArray: pad);
+
+            // We encrypt the bytes by adding our noise.
+            byte[] encrypted = Encrypt(originalBytes, pad);
+            // again, displaying in base64, but you would typically save
+            // these to a file too; this is your encrypted "file" or message.
+            textBoxEncrypted.Text = Convert.ToBase64String(inArray: encrypted);
+        }
+
+
+
+
+        public static byte[] GeneratePad(int size, int seed)
+        {
+            var random = new Random(Seed: seed);
+            var bytesBuffel = new byte[size];
+
+            random.NextBytes(bytesBuffel);
+
+            return bytesBuffel;
+        }
+        public static byte[] Encrypt(byte[] data, byte[] pad)
+        {
+            var result = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                var sum = (int)data[i] + (int)pad[i];
+                if (sum > 255)
+                    sum -= 255;
+                result[i] = (byte)sum;
+            }
+            return result;
+        }
+        public static byte[] Decrypt(byte[] encrypted, byte[] pad)
+        {
+            var result = new byte[encrypted.Length];
+            for (int i = 0; i < encrypted.Length; i++)
+            {
+                var dif = (int)encrypted[i] - (int)pad[i];
+                if (dif < 0)
+                    dif += 255;
+                result[i] = (byte)dif;
+            }
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            byte[] encryptedFromBase64 = Convert.FromBase64String(textBoxEncrypted.Text);
+
+            // decrypting the encoded message using the key made up of noise.
+            byte[] decrypted = Decrypt(encryptedFromBase64, pad);
+
+            // displaying the original unencrypted message.
+            textBoxDecrypted.Text = Encoding.Unicode.GetString(decrypted);
+        }
+    }
+}
